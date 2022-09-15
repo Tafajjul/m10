39b1eef2676d850337f275825275319f87ae3e7e
Tafajjul <tafazilani@gmail.com>
2022-09-15 17:57:50 +0530

    Array assignment

---
+++ Array_list.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+class Array_list
+{
+public static void main(String[] args)
+{
+ArrayList<String> l= new ArrayList<>();
+l.add("T");
+l.add("a");
+l.add("f");
+l.add("a");
+l.add("j");
+l.add("j");
+l.add("u");
+l.add("l");
+System.out.println(l);
+   }}
+
+
