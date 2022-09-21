e38165926a3e5bcd10c6bccdb6ae457437026444
Tafajjul <107162846+Tafajjul@users.noreply.github.com>
2022-09-21 17:22:15 +0530

    Add files via upload

---
+++ Auto_suggestion.java
@@ -0,0 +1,36 @@
+package practice;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Auto_suggestion {
+
+	public static void main(String[] args) throws InterruptedException {
+	System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.google.com");
+	driver.manage().window().maximize();
+	
+	driver.findElement(By.xpath("//input[@type='text']")).sendKeys("selenium");
+	Thread.sleep(3000);
+    List<WebElement>	suggestions=driver.findElements(By.xpath("//span[contains(text(),'selenium')]"));
+    System.out.println(suggestions.size());
+    
+    //suggestions.get(3).click();
+    for(int i =0; i<suggestions.size();i++)
+    {
+    	String text=suggestions.get(i).getText();
+    	if(text.equalsIgnoreCase("selenium webdriver"))
+    	{
+    		suggestions.get(i).click();
+    		break;
+    	}
+    }
+
+	}
+
+}
---
+++ Dropdown_list.java
@@ -0,0 +1,25 @@
+package practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Dropdown_list {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://money.rediff.com/tools/forex");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+	WebElement listbox	=driver.findElement(By.xpath("//select[@name='cnfrm']"));
+	
+	Select sel= new Select(listbox);
+	//sel.selectByVisibleText("UAE Dirham");
+	//sel.selectByValue("82.71");
+    sel.selectByIndex(10);
+	}
+
+}
